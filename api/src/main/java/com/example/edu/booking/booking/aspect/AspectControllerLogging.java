5500b6c2d7c4325c112a93a5ebb0a1916c32025c
ishibshi.futos <ishibshi.futos@outlook.com>
2020-04-17 17:18:51 +0900

    StopwatchをAspectで埋め込み

---
+++ api/src/main/java/com/example/edu/booking/booking/aspect/AspectControllerLogging.java
@@ -0,0 +1,42 @@
+package com.example.edu.booking.booking.aspect;
+
+import com.example.edu.booking.booking.utility.StopWatch;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+@Aspect
+@Component
+public class AspectControllerLogging {
+
+  private StopWatch stopWatch;
+
+  @Before("@annotation(org.springframework.web.bind.annotation.RequestMapping) "
+    + "|| @annotation(org.springframework.web.bind.annotation.GetMapping) "
+    + "|| @annotation(org.springframework.web.bind.annotation.PostMapping) "
+    + "|| @annotation(org.springframework.web.bind.annotation.PutMapping) "
+    + "|| @annotation(org.springframework.web.bind.annotation.DeleteMapping) "
+    + "|| @annotation(org.springframework.web.bind.annotation.PatchMapping)")
+  public void before(JoinPoint joinPoint) {
+    if (JoinPoint.METHOD_EXECUTION.equals(joinPoint.getKind())) {
+      var clazz = joinPoint.getTarget().getClass();
+      this.stopWatch = new StopWatch(clazz);
+    }
+  }
+
+  @After("@annotation(org.springframework.web.bind.annotation.RequestMapping) "
+    + "|| @annotation(org.springframework.web.bind.annotation.GetMapping) "
+    + "|| @annotation(org.springframework.web.bind.annotation.PostMapping) "
+    + "|| @annotation(org.springframework.web.bind.annotation.PutMapping) "
+    + "|| @annotation(org.springframework.web.bind.annotation.DeleteMapping) "
+    + "|| @annotation(org.springframework.web.bind.annotation.PatchMapping)")
+  public void after(JoinPoint jp) {
+    if (Objects.nonNull(this.stopWatch)) {
+      this.stopWatch.close();
+    }
+  }
+}
--- api/src/main/java/com/example/edu/booking/booking/utility/StopWatch.java
+++ api/src/main/java/com/example/edu/booking/booking/utility/StopWatch.java
@@ -4,24 +4,30 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.Closeable;
-import java.time.Instant;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 
 public class StopWatch implements Closeable {
   private final static Logger logger = LoggerFactory.getLogger(StopWatch.class);
   private final LocalDateTime startTime;
   private final String targetClassName;
+  private boolean closed;
+
   public StopWatch(Class<?> clazz) {
     this.targetClassName = clazz.getName();
+    this.closed = false;
     this.startTime = LocalDateTime.now();
     logger.debug("stopwatch start: target={}, startTime={}", this.targetClassName, this.startTime);
   }
 
   public void close() {
-    var endTime = LocalDateTime.now();
-    logger.debug("stopwatch stop: target={}, endTime={}", targetClassName, endTime);
-    logger.info("stopwatch diff: target={}, diff={}", targetClassName, ChronoUnit.MILLIS.between(startTime, endTime));
+    if (!this.closed) {
+      var endTime = LocalDateTime.now();
+      logger.debug("stopwatch stop: target={}, endTime={}", targetClassName, endTime);
+      logger.info("stopwatch diff: target={}, responseTime={}", targetClassName, ChronoUnit.MILLIS.between(startTime, endTime));
+      this.closed = true;
+    } else {
+      logger.warn("stopwatch closed.");
+    }
   }
 }
